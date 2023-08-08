735153004987876f1a329fa320c8da34d09d3a28
sharor1 <128407354+sharor1@users.noreply.github.com>
2023-08-08 22:50:42 +0300

    Add files via upload
    need to add checkout button thing to send the reservation to the database and alert thankyou for shopping with us and redirect to the homepage after purchase

--- client/scripts/checkout.js
+++ client/scripts/checkout.js
@@ -14,6 +14,7 @@ function updateCart() {
   total.textContent = `$${(cartTotal + 20).toFixed(2)}`;
   checkoutTotal.textContent = `$${(cartTotal + 20).toFixed(2)}`;
 }
+
 function createCartItem(name, price, description, imageUrl) {
   const cartItem = document.createElement('div');
   cartItem.className = 'card mb-3';
@@ -28,17 +29,9 @@ function createCartItem(name, price, description, imageUrl) {
           </div>
         </div>
         <div class="d-flex flex-row align-items-center">
-          <button class="btn btn-sm btn-outline-secondary" onclick="decrementCartItem('${name}', ${price})">-</button>
-          <div style="width: 50px">
-            <h5 class="fw-normal mb-0" id="${name}Quantity">1</h5>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary" onclick="incrementCartItem('${name}', ${price})">+</button>
           <div style="width: 80px">
             <h5 class="mb-0" id="${name}Price">$${price.toFixed(2)}</h5>
           </div>
-          <a href="#!" style="color: #cecece; margin-left: 10px;" onclick="removeCartItem('${name}', ${price})">
-            <i class="fas fa-trash-alt" aria-hidden="true"></i>
-          </a>
         </div>
       </div>
     </div>
@@ -50,44 +43,28 @@ function createCartItem(name, price, description, imageUrl) {
   updateCart();
 }
 
-
-
-function incrementCartItem(name, price) {
-  cartTotal += price;
-  const quantityElement = document.getElementById(`${name}Quantity`);
-  const priceElement = document.getElementById(`${name}Price`);
-  quantityElement.textContent = (parseInt(quantityElement.textContent) + 1).toString();
-  priceElement.textContent = `$${(parseFloat(priceElement.textContent.substring(1)) + price).toFixed(2)}`;
-  updateCart();
-}
-
-function decrementCartItem(name, price) {
-  if (itemsInCart > 0) {
-    cartTotal -= price;
-    const quantityElement = document.getElementById(`${name}Quantity`);
-    const priceElement = document.getElementById(`${name}Price`);
-    const quantity = parseInt(quantityElement.textContent);
-    if (quantity === 1) {
-      shoppingCart.removeChild(quantityElement.parentElement.parentElement.parentElement);
-      itemsInCart--;
-    } else {
-      quantityElement.textContent = (quantity - 1).toString();
-      priceElement.textContent = `$${(parseFloat(priceElement.textContent.substring(1)) - price).toFixed(2)}`;
-    }
-    updateCart();
-  }
-}
-
-function removeCartItem(name, price) {
-  const item = document.getElementById(`${name}Price`).parentElement.parentElement.parentElement;
-  cartTotal -= price * parseInt(item.querySelector(`#${name}Quantity`).textContent);
-  shoppingCart.removeChild(item);
-  itemsInCart--;
-  updateCart();
-}
-
 // Example items
-createCartItem('Iphone 11 pro', 900);
-createCartItem('Samsung galaxy Note 10', 900);
-createCartItem('Canon EOS M50', 1199);
-createCartItem('MacBook Pro', 1799);
+createCartItem(
+  'Iphone 11 pro',
+  900,
+  'High-end smartphone with advanced features.',
+  'https://example.com/iphone.jpg'
+);
+createCartItem(
+  'Samsung galaxy Note 10',
+  900,
+  'Premium Android smartphone with stylus.',
+  'https://example.com/samsung-galaxy.jpg'
+);
+createCartItem(
+  'Canon EOS M50',
+  1199,
+  'Mirrorless camera for photography enthusiasts.',
+  'https://example.com/canon-camera.jpg'
+);
+createCartItem(
+  'MacBook Pro',
+  1799,
+  'Powerful laptop for creative professionals.',
+  'https://example.com/macbook.jpg'
+);
