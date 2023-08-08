0de733a2f2e49f806d75a8df812dca6a58910168
sharor1 <128407354+sharor1@users.noreply.github.com>
2023-08-08 17:11:16 +0300

    Add files via upload

--- client/scripts/checkout.js
+++ client/scripts/checkout.js
@@ -1,17 +1,93 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Function to update the total price in the cart
-    function updateTotalPrice() {
-      const itemPrices = document.querySelectorAll(".item-price");
-      let totalPrice = 0;
-  
-      // Calculate the total price by summing up the prices of all items
-      itemPrices.forEach(function (price) {
-        totalPrice += parseFloat(price.innerText.substring(1)); // Convert price text to number
-      });
-  
-      // Update the total price in the cart
-      const totalPriceElement = document.querySelector(".cart-details li:last-child");
-      totalPriceElement.innerText = "Total: $" + totalPrice.toFixed(2);
+// JavaScript code for dynamic cart and buttons
+const shoppingCart = document.getElementById('shoppingCart');
+const cartItemCount = document.getElementById('cartItemCount');
+const subtotal = document.getElementById('subtotal');
+const total = document.getElementById('total');
+const checkoutTotal = document.getElementById('checkoutTotal');
+
+let itemsInCart = 0;
+let cartTotal = 0;
+
+function updateCart() {
+  cartItemCount.querySelector('span').textContent = itemsInCart;
+  subtotal.textContent = `$${cartTotal.toFixed(2)}`;
+  total.textContent = `$${(cartTotal + 20).toFixed(2)}`;
+  checkoutTotal.textContent = `$${(cartTotal + 20).toFixed(2)}`;
+}
+function createCartItem(name, price, description, imageUrl) {
+  const cartItem = document.createElement('div');
+  cartItem.className = 'card mb-3';
+  cartItem.innerHTML = `
+    <div class="card-body">
+      <div class="d-flex justify-content-between">
+        <div class="d-flex flex-row align-items-center">
+          <img src="${imageUrl}" class="img-fluid rounded-circle" style="width: 45px" alt="Item Image" />
+          <div class="ms-3">
+            <h5>${name}</h5>
+            <p class="small mb-0">${description}</p>
+          </div>
+        </div>
+        <div class="d-flex flex-row align-items-center">
+          <button class="btn btn-sm btn-outline-secondary" onclick="decrementCartItem('${name}', ${price})">-</button>
+          <div style="width: 50px">
+            <h5 class="fw-normal mb-0" id="${name}Quantity">1</h5>
+          </div>
+          <button class="btn btn-sm btn-outline-secondary" onclick="incrementCartItem('${name}', ${price})">+</button>
+          <div style="width: 80px">
+            <h5 class="mb-0" id="${name}Price">$${price.toFixed(2)}</h5>
+          </div>
+          <a href="#!" style="color: #cecece; margin-left: 10px;" onclick="removeCartItem('${name}', ${price})">
+            <i class="fas fa-trash-alt" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  `;
+
+  shoppingCart.appendChild(cartItem);
+  itemsInCart++;
+  cartTotal += price;
+  updateCart();
+}
+
+
+
+function incrementCartItem(name, price) {
+  cartTotal += price;
+  const quantityElement = document.getElementById(`${name}Quantity`);
+  const priceElement = document.getElementById(`${name}Price`);
+  quantityElement.textContent = (parseInt(quantityElement.textContent) + 1).toString();
+  priceElement.textContent = `$${(parseFloat(priceElement.textContent.substring(1)) + price).toFixed(2)}`;
+  updateCart();
+}
+
+function decrementCartItem(name, price) {
+  if (itemsInCart > 0) {
+    cartTotal -= price;
+    const quantityElement = document.getElementById(`${name}Quantity`);
+    const priceElement = document.getElementById(`${name}Price`);
+    const quantity = parseInt(quantityElement.textContent);
+    if (quantity === 1) {
+      shoppingCart.removeChild(quantityElement.parentElement.parentElement.parentElement);
+      itemsInCart--;
+    } else {
+      quantityElement.textContent = (quantity - 1).toString();
+      priceElement.textContent = `$${(parseFloat(priceElement.textContent.substring(1)) - price).toFixed(2)}`;
     }
-  });
- 
+    updateCart();
+  }
+}
+
+function removeCartItem(name, price) {
+  const item = document.getElementById(`${name}Price`).parentElement.parentElement.parentElement;
+  cartTotal -= price * parseInt(item.querySelector(`#${name}Quantity`).textContent);
+  shoppingCart.removeChild(item);
+  itemsInCart--;
+  updateCart();
+}
+
+// Example items
+createCartItem('Iphone 11 pro', 900);
+createCartItem('Samsung galaxy Note 10', 900);
+createCartItem('Canon EOS M50', 1199);
+createCartItem('MacBook Pro', 1799);
