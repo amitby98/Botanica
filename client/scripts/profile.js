234f424f16fce1e897fb98950ca974545c78af47
amitby98 <77382577+amitby98@users.noreply.github.com>
2023-08-11 22:53:28 +0300

    added profile functionality

--- client/scripts/profile.js
+++ client/scripts/profile.js
@@ -1,28 +1,69 @@
-//Add new order
-function addOrder(date, content, total) {
-  const container = document.getElementById("order-container");
-
-  const divBox = document.createElement("div");
-  divBox.classList.add("div-box");
-
-  const dateDiv = document.createElement("div");
-  dateDiv.classList.add("Date");
-  const dateH3 = document.createElement("h3");
-  dateH3.textContent = date;
-  dateDiv.appendChild(dateH3);
-
-  const contentP = document.createElement("p");
-  contentP.textContent = content;
-
-  const totalH3 = document.createElement("h3");
-  totalH3.textContent = total;
-
-  divBox.appendChild(dateDiv);
-  divBox.appendChild(contentP);
-  divBox.appendChild(totalH3);
-
-  container.appendChild(divBox);
+let renderedOrders = [];
+function addOrder(orders) {
+  renderedOrders = orders;
+  $("#order-container").html("");
+  for (const order of orders) {
+    $("#order-container").append(`
+      <div class="div-box">
+        <div class="date">
+        <h3>${order.timestamps}</h3>
+        </div>
+        <p>${order.items}</p>
+        <h3>${order.total}</h3>
+      </div>
+  `);
+  }
 }
 
-// הוספת ריבוע עם תוכן חדש
-addOrder();
+//Add new order
+// function addOrder(date, content, total) {
+//   const container = document.getElementById("order-container");
+
+//   const divBox = document.createElement("div");
+//   divBox.classList.add("div-box");
+
+//   const dateDiv = document.createElement("div");
+//   dateDiv.classList.add("date");
+//   const dateH3 = document.createElement("h3");
+//   dateH3.textContent = date;
+//   dateDiv.appendChild(dateH3);
+
+//   const contentP = document.createElement("p");
+//   contentP.textContent = content;
+
+//   const totalH3 = document.createElement("h3");
+//   totalH3.textContent = total;
+
+//   divBox.appendChild(dateDiv);
+//   divBox.appendChild(contentP);
+//   divBox.appendChild(totalH3);
+
+//   container.appendChild(divBox);
+// }
+
+// addOrder();
+
+// $.ajax({
+//   url: "api/orders/:userId",
+//   method: "GET",
+//   success: data => {
+//     console.log(data);
+//     addOrder(data.orders);
+//   },
+// });
+
+const userId = "64d606938d7ea4db3ab2508d";
+
+$.ajax({
+  url: "api/orders/" + userId,
+  method: "GET",
+  success: data => {
+    console.log(data);
+    // data.orders.forEach(order => {
+    addOrder(order.items, order.total, order.userId);
+    // });
+  },
+  error: (xhr, status, error) => {
+    console.error(error);
+  },
+});
